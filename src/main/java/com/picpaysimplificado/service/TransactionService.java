a3bb52454cffde6f2f7843fba052d4d809d0e524
Anailson Ribeiro dos Santos <anailson.santos@corregedoria.df>
2023-10-25 12:10:29 -0300

    feat - Testes Unitários - Junit + Mockito

--- src/main/java/com/picpaysimplificado/service/TransactionService.java
+++ src/main/java/com/picpaysimplificado/service/TransactionService.java
@@ -5,14 +5,9 @@ import com.picpaysimplificado.domain.user.User;
 import com.picpaysimplificado.dtos.TransactionDTO;
 import com.picpaysimplificado.repositories.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
-import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.util.Map;
 
 @Service
 public class TransactionService {
@@ -24,10 +19,10 @@ public class TransactionService {
     private TransactionRepository repository;
 
     @Autowired
-    private RestTemplate restTemplate;
+    private NotificationService notificationService;
 
     @Autowired
-    private NotificationService notificationService;
+    private AuthorizeService authorizeService;
 
     public Transaction createTransaction(TransactionDTO transaction) throws Exception {
         User sender = this.userService.findUserById(transaction.senderId());
@@ -35,7 +30,7 @@ public class TransactionService {
 
         userService.validarTransaction(sender, transaction.value());
 
-        boolean isAuthorized = !this.authorizeTransaction(sender, transaction.value());
+        boolean isAuthorized = !this.authorizeService.authorizeTransaction(sender, transaction.value());
         if(isAuthorized){
             throw  new Exception("Transação não autorizada");
         }
@@ -59,18 +54,6 @@ public class TransactionService {
 
         return newTransaction;
 
-    }
-
-
-    public boolean authorizeTransaction(User sender, BigDecimal value){
-       ResponseEntity<Map> authorizationResponse = restTemplate.getForEntity("https://run.mocky.io/v3/5794d450-d2e2-4412-8131-73d0293ac1cc", Map.class);
-       //novo api https://run.mocky.io/v3/5794d450-d2e2-4412-8131-73d0293ac1cc
-        //https://run.mocky.io/v3/8fafdd68-a090-496f-8c9a-3442cf30dae6 -> antiga
-
-       if(authorizationResponse.getStatusCode() == HttpStatus.OK){
-           String message = (String) authorizationResponse.getBody().get("message");
-           return  "Autorizado".equalsIgnoreCase(message);
-       }else return false;
     }
 
 }
